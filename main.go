f61f93b9017672b6533c094b3afbc0e4ba52e30e
KreakxX <Henrik.standke@web.de>
2025-11-10 20:33:06 +0100

    basic Websocket server done

--- main.go
+++ main.go
@@ -1,25 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-func main() {
-	fmt.Println("Hello Go")
+	"github.com/gorilla/websocket"
+)
 
-	var x int = 10
-	x += 10
-
-	fmt.Println(x)
-
-	for i := 0; i < 5; i++ {
-		fmt.Print(i)
-	}
-
-	go func() {
-		fmt.Println("hello")
-	}()
-
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
-func add(a, b int) int {
-	return a + b
+func ws_handler(w http.ResponseWriter, r *http.Request) {
+	// upgrade current connection to websocket connection
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error while upgrading", err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error while reading messages from client", err)
+			break
+		}
+
+		fmt.Println(message)
+		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			fmt.Println("Error writing message:", err)
+			break
+		}
+
+	}
+}
+
+func main() {
+	http.HandleFunc("/", ws_handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Print("error while trying to start the Server", err)
+	}
 }
